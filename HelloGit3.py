b8674ae0dcf92b35034117ac0ac91d634616ce87
Admin <diegovaloriza@gmail.com>
2024-02-20 16:04:33 +0100

    Guardado despues del STASH

--- HelloGit3.py
+++ HelloGit3.py
@@ -1,5 +1,6 @@
 print ("Apredndiendo2")
 print ("Apredndiendo2")
 print ("Apredndiendo2")
-
+print ("PRINT STASH / git stash")
+print ("Guardado desde la rama login")
 print ("Guardado desde la rama login")
