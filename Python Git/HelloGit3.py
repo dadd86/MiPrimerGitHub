5a1d9ea09299335557d0cb8ccaa2b0d55d96f504
Admin <diegovaloriza@gmail.com>
2024-02-27 16:32:51 +0100

    modificacion de archivos

---
+++ Python Git/HelloGit3.py
@@ -0,0 +1,7 @@
+print ("Apredndiendo2")
+print ("Apredndiendo2")
+print ("Apredndiendo2")
+print ("PRINT STASH / git stash")
+print ("Guardado desde la rama login")
+print ("Guardado desde la rama login")
+print ("Prueba Github")
